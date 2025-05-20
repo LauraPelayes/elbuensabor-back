9b8c3ad2f910ba8e457fca3b61940b90e0258d36
KOmoROid <135452272+KOmoROid@users.noreply.github.com>
2025-05-20 16:55:06 -0300

    Update UsuarioRepository.java

--- ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Repositories/UsuarioRepository.java
+++ ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Repositories/UsuarioRepository.java
@@ -3,6 +3,6 @@ package ElBuenSabor.ProyectoFinal.Repositories;
 import ElBuenSabor.ProyectoFinal.Entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Usuario findByUsername(String username);
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> { // Cambiado Integer a Long
+    Usuario findByUsername(String username); // Añade este método si aún no lo has hecho
 }
